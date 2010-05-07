54f02b6f204b55b15b5130aec0f407aee26cd5a9
nickdos <nickdos@gmail.com>
2010-05-07 06:27:08 +0000

    Updated SQL & schema.xml to handle more fields.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-webapp@733 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- db/occurrences.sql
+++ db/occurrences.sql
@@ -5,7 +5,7 @@
 SELECT 'id','data_provider_id','data_provider','data_resource_id','data_resource',
 'institution_code_id','institution_code','institution_code_name','institution_code_lsid',
 'collection_code_id','collection_code','catalogue_number_id','catalogue_number',
-'taxon_concept_lsid','taxon_name','author','raw_taxon_name','raw_author','country_code',
+'taxon_concept_lsid','taxon_name','author','rank','rank_id','raw_taxon_name','raw_author','country_code',
 'kingdom_lsid','kingdom','family_lsid','family',
 'state',
 'biogeographic_region',
@@ -18,19 +18,20 @@ UNION
 SELECT oc.id, oc.data_provider_id, dp.name data_provider_name, oc.data_resource_id, dr.name data_resource_name,
 oc.institution_code_id, ic.code institution_code_code, ic.name institution_code_name, ic.lsid institution_code_lsid,
 oc.collection_code_id, cc.code collection_code, oc.catalogue_number_id, cn.code catalogue_number, 
-tc.lsid, tn.canonical taxon_name, tn.author, ror.scientific_name, ror.author raw_author, oc.iso_country_code,
+tc.lsid, tn.canonical taxon_name, tn.author, tc.rank, rnk.`name`, ror.scientific_name, ror.author raw_author, oc.iso_country_code,
 ktc.lsid, ktn.canonical, ftc.lsid, ftn.canonical,
 GROUP_CONCAT(st.name ORDER BY st.name SEPARATOR "|") as states,
 GROUP_CONCAT(bgr.name ORDER BY bgr.name SEPARATOR "|") as bio_geo_regions,
 GROUP_CONCAT(plc.name ORDER BY plc.name SEPARATOR "|") as places,
 oc.latitude, oc.longitude, ror.lat_long_precision, oc.cell_id, oc.centi_cell_id, oc.tenmilli_cell_id,
-oc.year, oc.month, oc.occurrence_date, oc.basis_of_record basis_of_record_id, bor.description basis_of_record, ror.basis_of_record,
-typ.type_status, lit.it_value, idr.identifier, ror.identifier_name, ror.identification_date,
+oc.year, oc.month, DATE_FORMAT(oc.occurrence_date,'%Y-%m-%dT%H:%i:%sZ'), oc.basis_of_record basis_of_record_id, bor.description basis_of_record, ror.basis_of_record,
+typ.type_status, lit.it_value, idr.identifier, ror.identifier_name, DATE_FORMAT(ror.identification_date,'%Y-%m-%dT%H:%i:%sZ'),
 ror.collector_name, oc.taxonomic_issue, oc.geospatial_issue, oc.other_issue, ror.created, ror.modified
 FROM occurrence_record oc
 INNER JOIN raw_occurrence_record ror ON ror.id = oc.id
 INNER JOIN taxon_name tn ON tn.id = oc.taxon_name_id
 INNER JOIN taxon_concept tc ON tc.id = oc.nub_concept_id
+INNER JOIN rank rnk ON rnk.id = tc.rank
 INNER JOIN taxon_concept ktc ON ktc.id = oc.kingdom_concept_id
 INNER JOIN taxon_name ktn ON ktn.id = ktc.taxon_name_id
 INNER JOIN taxon_concept ftc ON ftc.id = oc.family_concept_id
