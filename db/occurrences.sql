f7dcb7356d3fd27e0b2b1ebf5aeb4bb18b67e4a2
nickdos <nickdos@gmail.com>
2010-05-13 10:36:35 +0000

    Uses new schema for portal db. Added all higher taxa. Changed all joins to inner join to see if it runs faster (based on StackOverflow posts).
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-webapp@741 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- db/occurrences.sql
+++ db/occurrences.sql
@@ -6,7 +6,8 @@ SELECT 'id','data_provider_id','data_provider','data_resource_id','data_resource
 'institution_code_id','institution_code','institution_code_name','institution_code_lsid',
 'collection_code_id','collection_code','catalogue_number_id','catalogue_number',
 'taxon_concept_lsid','taxon_name','author','rank_id','rank','raw_taxon_name','raw_author','country_code',
-'kingdom_lsid','kingdom','family_lsid','family',
+'kingdom_lsid','kingdom','phylum_lsid','phylum','class_lsid','class','order_lsid','order',
+'family_lsid','family','genus_lsid','genus','species_lsid','species',
 'state',
 'biogeographic_region',
 'places',
@@ -18,8 +19,9 @@ UNION
 SELECT oc.id, oc.data_provider_id, dp.name data_provider_name, oc.data_resource_id, dr.name data_resource_name,
 oc.institution_code_id, ic.code institution_code_code, ic.name institution_code_name, ic.lsid institution_code_lsid,
 oc.collection_code_id, cc.code collection_code, oc.catalogue_number_id, cn.code catalogue_number, 
-tc.lsid, tn.canonical taxon_name, tn.author, tc.rank, rnk.`name`, ror.scientific_name, ror.author raw_author, oc.iso_country_code,
-ktc.lsid, ktn.canonical, ftc.lsid, ftn.canonical,
+tc.guid, tn.canonical taxon_name, tn.author, tc.rank, rnk.`name`, ror.scientific_name, ror.author raw_author, oc.iso_country_code,
+kdc.guid, kdn.canonical, phc.guid, phn.canonical, clc.guid, cln.canonical, odc.guid, odn.canonical,
+fmc.guid, fmn.canonical, gnc.guid, gnn.canonical, spc.guid, spn.canonical,
 GROUP_CONCAT(st.name ORDER BY st.name SEPARATOR "|") as states,
 GROUP_CONCAT(bgr.name ORDER BY bgr.name SEPARATOR "|") as bio_geo_regions,
 GROUP_CONCAT(plc.name ORDER BY plc.name SEPARATOR "|") as places,
@@ -28,20 +30,30 @@ oc.year, oc.month, DATE_FORMAT(oc.occurrence_date,'%Y-%m-%dT%H:%i:%sZ'), oc.basi
 typ.type_status, lit.it_value, idr.identifier, ror.identifier_name, DATE_FORMAT(ror.identification_date,'%Y-%m-%dT%H:%i:%sZ'),
 ror.collector_name, oc.taxonomic_issue, oc.geospatial_issue, oc.other_issue, ror.created, ror.modified
 FROM occurrence_record oc
-INNER JOIN raw_occurrence_record ror ON ror.id = oc.id
-INNER JOIN taxon_name tn ON tn.id = oc.taxon_name_id
-INNER JOIN taxon_concept tc ON tc.id = oc.nub_concept_id
-INNER JOIN rank rnk ON rnk.id = tc.rank
-INNER JOIN taxon_concept ktc ON ktc.id = oc.kingdom_concept_id
-INNER JOIN taxon_name ktn ON ktn.id = ktc.taxon_name_id
-INNER JOIN taxon_concept ftc ON ftc.id = oc.family_concept_id
-INNER JOIN taxon_name ftn ON ftn.id = ftc.taxon_name_id
-INNER JOIN data_provider dp ON dp.id = oc.data_provider_id
-INNER JOIN data_resource dr ON dr.id = oc.data_resource_id
-INNER JOIN institution_code ic ON ic.id = oc.institution_code_id
-INNER JOIN collection_code cc ON cc.id = oc.collection_code_id
-INNER JOIN catalogue_number cn ON cn.id = oc.catalogue_number_id
-INNER JOIN basis_of_record bor ON bor.id = oc.basis_of_record
+LEFT JOIN raw_occurrence_record ror ON ror.id = oc.id
+LEFT JOIN taxon_name tn ON tn.id = oc.taxon_name_id
+LEFT JOIN taxon_concept tc ON tc.id = oc.nub_concept_id
+LEFT JOIN rank rnk ON rnk.id = tc.rank
+LEFT JOIN taxon_concept kdc ON kdc.id = oc.kingdom_concept_id
+LEFT JOIN taxon_name kdn ON kdn.id = kdc.taxon_name_id
+LEFT JOIN taxon_concept phc ON phc.id = oc.phylum_concept_id
+LEFT JOIN taxon_name phn ON phn.id = phc.taxon_name_id
+LEFT JOIN taxon_concept clc ON clc.id = oc.class_concept_id
+LEFT JOIN taxon_name cln ON cln.id = clc.taxon_name_id
+LEFT JOIN taxon_concept odc ON odc.id = oc.order_concept_id
+LEFT JOIN taxon_name odn ON odn.id = odc.taxon_name_id
+LEFT JOIN taxon_concept fmc ON fmc.id = oc.family_concept_id
+LEFT JOIN taxon_name fmn ON fmn.id = fmc.taxon_name_id
+LEFT JOIN taxon_concept gnc ON gnc.id = oc.genus_concept_id
+LEFT JOIN taxon_name gnn ON gnn.id = gnc.taxon_name_id
+LEFT JOIN taxon_concept spc ON spc.id = oc.species_concept_id
+LEFT JOIN taxon_name spn ON spn.id = spc.taxon_name_id
+LEFT JOIN data_provider dp ON dp.id = oc.data_provider_id
+LEFT JOIN data_resource dr ON dr.id = oc.data_resource_id
+LEFT JOIN institution_code ic ON ic.id = oc.institution_code_id
+LEFT JOIN collection_code cc ON cc.id = oc.collection_code_id
+LEFT JOIN catalogue_number cn ON cn.id = oc.catalogue_number_id
+LEFT JOIN basis_of_record bor ON bor.id = oc.basis_of_record
 LEFT JOIN geo_mapping gm ON gm.occurrence_id = oc.id
 LEFT JOIN geo_region st ON st.id = gm.geo_region_id AND st.region_type <= 2
 LEFT JOIN geo_region bgr ON bgr.id = gm.geo_region_id AND bgr.region_type = 2000
@@ -52,6 +64,5 @@ LEFT JOIN lookup_identifier_type lit ON lit.it_key = idr.identifier_type
 --WHERE oc.data_resource_id = 56
 GROUP BY oc.id
 INTO outfile '/data/bie-staging/biocache/occurrences.csv'
+--INTO outfile '/data/bie-staging/biocache/occurrences.56.csv'
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'; -- ESCAPED BY '"'
-
--- WHERE oc.data_resource_id = 56
