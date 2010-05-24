26edba73d56b87f1f522282fa1283838e5b99770
nickdos <nickdos@gmail.com>
2010-05-24 23:52:35 +0000

    Fixed occurrence dump file name.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-webapp@788 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- solr_conf/update_solr_index.sh
+++ solr_conf/update_solr_index.sh
@@ -4,7 +4,7 @@ echo "deleting existing index..."
 curl 'http://localhost:8080/solr/update?commit=true -H "Content-Type: text/xml" --data-binary "<delete><query>id:[* TO *]</query></delete>"date'
 echo "indexing from CSV file..."
 date
-curl  'http://localhost:8080/solr/update/csv?commit=true&f.state.split=true&f.state.separator=|&f.biogeographic_region.split=true&f.biogeographic_region.separator=|&f.places.split=true&f.places.separator=|&map=\N:&trim=true&overwrite=false&stream.file=/data/bie-staging/biocache/occurrences.fixed2.csv'
+curl  'http://localhost:8080/solr/update/csv?commit=true&f.state.split=true&f.state.separator=|&f.biogeographic_region.split=true&f.biogeographic_region.separator=|&f.places.split=true&f.places.separator=|&map=\N:&trim=true&overwrite=false&stream.file=/data/bie-staging/biocache/occurrences.csv'
 date
 # commit
 #curl http://localhost:8080/solr/update?stream.body=%3Ccommit/%3E
