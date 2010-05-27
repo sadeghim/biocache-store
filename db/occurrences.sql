a33192085f3473211c1cb06bca06c9a31e508be2
nickdos <nickdos@gmail.com>
2010-05-27 23:21:17 +0000

    Added 4 levels of point coords to SOLR index (0.1 deg to 0.0001 deg)
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-webapp@801 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- db/occurrences.sql
+++ db/occurrences.sql
@@ -123,9 +123,10 @@ SELECT 'id','data_provider_id','data_provider','data_resource_id','data_resource
 'places',
 'latitude','longitude','lat_long_precision','lat_long',
 'cell_id','centi_cell_id','tenmilli_cell_id',
+'point-0.1','point-0.01','point-0.001','point-0.0001',
 'year','month','occurrence_date','basis_of_record_id','basis_of_record','raw_basis_of_record',
 'type_status','identifier_type','identifier_value','identifier_name','identifier_date',
-'collector','taxonomic_issue','geospatial_issue','other_issue','created_date','modified_date','point'
+'collector','taxonomic_issue','geospatial_issue','other_issue','created_date','modified_date'
 UNION
 SELECT IFNULL(oc.id,''),IFNULL(oc.data_provider_id,''),IFNULL(dp.`name`,''),IFNULL(oc.data_resource_id,''),
 IFNULL(dr.`name`,''),IFNULL(oc.institution_code_id,''),IFNULL(ic.code,''),IFNULL(ic.`name`,''),
@@ -145,13 +146,16 @@ IFNULL(get_georegion_names(oc.id,3,11),'') as places,
 IFNULL(oc.latitude,''),IFNULL(oc.longitude,''),IFNULL(ror.lat_long_precision,''),
 IFNULL(CONCAT_WS(',', oc.latitude, oc.longitude),'') as lat_long,
 IFNULL(oc.cell_id,''),IFNULL(oc.centi_cell_id,''),IFNULL(oc.tenmilli_cell_id,''),
+CONCAT(CONCAT(round(oc.latitude *10)/10, ','), round(oc.longitude *10)/10),
+CONCAT(CONCAT(round(oc.latitude *100)/100, ','), round(oc.longitude *100)/100),
+CONCAT(CONCAT(round(oc.latitude *1000)/1000, ','), round(oc.longitude *1000)/1000),
+CONCAT(CONCAT(round(oc.latitude *10000)/10000, ','), round(oc.longitude *10000)/10000)
 IFNULL(oc.`year`,''),IFNULL(oc.`month`,''),IFNULL(DATE_FORMAT(oc.occurrence_date,'%Y-%m-%dT%H:%i:%sZ'),''),
 IFNULL(oc.basis_of_record,''),IFNULL(bor.description,''),IFNULL(ror.basis_of_record,''),
 IFNULL(get_type_status(oc.id),''),IFNULL(get_identifier_type(oc.id),''),IFNULL(get_identifier_name(oc.id),''),
 IFNULL(ror.identifier_name,''),IFNULL(DATE_FORMAT(ror.identification_date,'%Y-%m-%dT%H:%i:%sZ'),''),
 IFNULL(ror.collector_name,''),IFNULL(oc.taxonomic_issue,''),IFNULL(oc.geospatial_issue,''),
-IFNULL(oc.other_issue,''),IFNULL(DATE_FORMAT(ror.created,'%Y-%m-%dT%H:%i:%sZ'),''),IFNULL(DATE_FORMAT(ror.modified,'%Y-%m-%dT%H:%i:%sZ'),''),
-CONCAT(CONCAT(round(oc.latitude *10000)/10000, '|'), round(oc.longitude *10000)/10000)
+IFNULL(oc.other_issue,''),IFNULL(DATE_FORMAT(ror.created,'%Y-%m-%dT%H:%i:%sZ'),''),IFNULL(DATE_FORMAT(ror.modified,'%Y-%m-%dT%H:%i:%sZ'),'')
 FROM occurrence_record oc
 INNER JOIN raw_occurrence_record ror ON ror.id = oc.id
 INNER JOIN taxon_name tn ON tn.id = oc.taxon_name_id
