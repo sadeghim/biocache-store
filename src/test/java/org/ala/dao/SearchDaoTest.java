dadda714828044bb5f2a2bffb22205924ccb4177
djtfmartin <djtfmartin@gmail.com>
2010-07-21 01:38:55 +0000

    additional methods for JSON calls to biocache
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-webapp@948 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/org/ala/dao/SearchDaoTest.java
+++ src/test/java/org/ala/dao/SearchDaoTest.java
@@ -6,6 +6,9 @@ import junit.framework.TestCase;
 
 import org.ala.biocache.dao.SearchDao;
 import org.ala.biocache.model.DataProviderCountDTO;
+import org.ala.biocache.model.FieldResultDTO;
+import org.ala.biocache.model.SearchQuery;
+import org.ala.biocache.util.SearchUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -18,6 +21,22 @@ public class SearchDaoTest extends TestCase {
 		List<DataProviderCountDTO> countDTOs = searchDao.getDataProviderCounts();
 		
 		for(DataProviderCountDTO countDTO: countDTOs){
+			System.out.println(countDTO);
+		}
+	}
+	
+	public void testCollectionByDecade() throws Exception {
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext(new String[]{"classpath:applicationContext-webapp.xml"});
+		SearchDao searchDao = (SearchDao) ac.getBean("searchDao");
+		
+		SearchUtils s = new SearchUtils();
+		SearchQuery searchQuery = new SearchQuery("urn:lsid:biocol.org:col:32981", "collection");
+		s.updateCollectionSearchString(searchQuery);
+		
+		List<FieldResultDTO> countDTOs = searchDao.findRecordByDecadeFor(searchQuery.getQuery());
+		
+		for(FieldResultDTO countDTO: countDTOs){
 			System.out.println(countDTO);
 		}
 	}
