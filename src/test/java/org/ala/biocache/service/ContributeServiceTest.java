8c57a510ccfb2564de5ff5176f04d775b784884e
djtfmartin <djtfmartin@gmail.com>
2010-08-24 12:28:20 +0000

    contribute sighting
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-webapp@1071 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ src/test/java/org/ala/biocache/service/ContributeServiceTest.java
@@ -0,0 +1,34 @@
+package org.ala.biocache.service;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+
+import org.ala.biocache.dto.Sighting;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ContributeServiceTest extends TestCase {
+
+	public void testRecordSighting(){
+		
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"classpath*:applicationContext-webapp.xml"});
+		ContributeService cs = ctx.getBean(ContributeService.class);
+		Sighting s = new Sighting();
+		s.setLocality("Clunes Ross");
+		s.setStateProvince("Australian Capital Territory");
+		s.setCollectorName("David Martin");
+		s.setScientificName("Macropus rufus");
+		s.setVernacularName("Red kangaroo");
+		s.setFamily("Macropodidae");
+		s.setLatitude(-37.12f);
+		s.setLongitude(123.12f);
+		s.setCoordinateUncertaintyInMeters(100);
+		s.setRank("species");
+		s.setTaxonConceptGuid("urn:lsid:biodiversity.org.au:afd.taxon:aa745ff0-c776-4d0e-851d-369ba0e6f537");
+		s.setUserId("David.Martin@csiro.au");
+		s.setOccurrenceRemarks("It was a cold dark night....");
+		s.setEventDate(new Date());
+		cs.recordSighting(s);
+	}
+}
