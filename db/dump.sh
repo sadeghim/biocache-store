d13feb5fc3ebddb4946eb4af7a0254a597e169ac
natasha.carter@csiro.au <natasha.carter@csiro.au>
2010-08-26 00:51:22 +0000

    Modified the occurrences.sql script so that the records are dumped in batches.
    git-svn-id: http://ala-portal.googlecode.com/svn/trunk/biocache-webapp@1107 1ef1b718-b4f5-11dd-90a5-ed332e521da9

---
+++ db/dump.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo 'Running occurrences.sql against portal...'
+mysql -uroot -ppassword portal < /data/bie-staging/biocache/db/occurrences.sql
+date
+echo removing empty files
+find /data/bie-staging/biocache/tmp -type f -size 0 -exec rm -f {} \;
+date
+echo Concat files
+cat /data/bie-staging/biocache/tmp/header.txt /data/bie-staging/biocache/tmp/file.out.* > /data/bie-staging/biocache/occurrences-tmp.csv
+date
+
+echo "Fixing escaping of quotes..."
+date
+sed -e 's/\\"/""/g' /data/bie-staging/biocache/occurrences-tmp.csv > /data/bie-staging/biocache/occurrences.tmp2.csv
+sed -e 's/\\""/\\"/g' /data/bie-staging/biocache/occurrences.tmp2.csv > /data/bie-staging/biocache/occurrences.csv
+
+date
+echo "Script has finished."
--- db/occurrences.sql
+++ db/occurrences.sql
@@ -55,7 +55,6 @@ READS SQL DATA
     END $$
 
 -- Stored function to lookup taxon name for a given higher taxa concept id.
---NC the conversions occur to ensure that the name is a valid utf-8 string
 -- Nick dos Remedios 2010-05-16
 -- Args:
 -- concept_id: the input concept id
@@ -65,7 +64,7 @@ DETERMINISTIC
 READS SQL DATA
     BEGIN
         DECLARE canonical VARCHAR(1000);
-        SELECT convert(convert(convert(tn.canonical USING latin1) USING binary) using utf8)
+        SELECT tn.canonical
         INTO canonical
         FROM taxon_concept tc
         INNER JOIN taxon_name tn ON tn.id = tc.taxon_name_id
@@ -129,67 +128,84 @@ READS SQL DATA
         RETURN identifier_name;
     END $$
 
+-- A recursive stored procedure that dumps the occurrences
+-- records to mulitple temporary files. This procedure improves 
+-- the dump  time.  
+-- Natasha Carter 2010-08-25
+-- Args:
+-- low: The minimum id of the occurrence record to dump
+-- high: One less than the maximum occurrence record to dump
+DROP PROCEDURE IF EXISTS dump_bio_occurrences$$
+CREATE PROCEDURE dump_bio_occurrences(IN low INT, IN high INT) 
+	BEGIN
+		DECLARE more BOOLEAN;
+		set @cmd = concat('SELECT STRAIGHT_JOIN oc.id,IFNULL(dp.uid,\'\'),IFNULL(dp.`name`,\'\'),IFNULL(dr.uid,\'\'),'
+		,'IFNULL(dr.`name`,\'\'),IFNULL(icm.institution_uid,\'\'),IFNULL(ic.code,\'\'),IFNULL(ic.`name`,\'\'),'
+		,'IFNULL(ic.lsid,\'\'),IFNULL(icm.collection_uid,\'\'),IFNULL(cc.code,\'\'),IFNULL(oc.catalogue_number_id,\'\'),'
+		,'IFNULL(cn.code,\'\'),IFNULL(tc.guid,\'\'),IFNULL(tn.canonical,\'\'),IFNULL(tn.author,\'\'), IFNULL(get_first_taxa_cn(oc.taxon_concept_id),\'\'),'
+		,'CONCAT_WS(\'|\', IFNULL(tn.canonical,\'\'), IFNULL(tc.guid,\'\'), IFNULL(get_first_taxa_cn(oc.taxon_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.kingdom_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.family_concept_id),\'\') ),'
+		,'IFNULL(tc.rank,\'\'), IFNULL(rnk.`name`,\'\'),IFNULL(ror.scientific_name,\'\'),IFNULL(ror.author,\'\'),'
+		,'IFNULL(oc.iso_country_code,\'\'), IFNULL(tc.lft,\'\'), IFNULL(tc.rgt,\'\'),'
+		,'IFNULL(get_taxa_guid(oc.kingdom_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.kingdom_concept_id),\'\'),'
+		,'IFNULL(get_taxa_guid(oc.phylum_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.phylum_concept_id),\'\'),'
+		,'IFNULL(get_taxa_guid(oc.class_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.class_concept_id),\'\'),'
+		,'IFNULL(get_taxa_guid(oc.order_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.order_concept_id),\'\'),'
+		,'IFNULL(get_taxa_guid(oc.family_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.family_concept_id),\'\'),'
+		,'IFNULL(get_taxa_guid(oc.genus_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.genus_concept_id),\'\'),'
+		,'IFNULL(get_taxa_guid(oc.species_concept_id),\'\'), IFNULL(get_taxa_canonical(oc.species_concept_id),\'\'),'
+		,'IFNULL(get_georegion_names(oc.id,0,2),\'\') as states,'
+		,'IFNULL(get_georegion_names(oc.id,2000,3999),\'\') as bio_geo_regions,'
+		,'IFNULL(get_georegion_names(oc.id,3,11),\'\') as places,'
+		,'IFNULL(oc.latitude,\'\'),IFNULL(oc.longitude,\'\'),IFNULL(ror.lat_long_precision,\'\'),'
+		,'IFNULL(CONCAT_WS(\'\,\', oc.latitude, oc.longitude),\'\') as lat_long,'
+		,'IFNULL(oc.cell_id,\'\'),IFNULL(oc.centi_cell_id,\'\'),IFNULL(oc.tenmilli_cell_id,\'\'),'
+		,'CONCAT(CONCAT(ROUND(round(oc.latitude *1)/1,1), \'\,\'), ROUND(round(oc.longitude *1)/1,1)),'
+		,'CONCAT(CONCAT(ROUND(round(oc.latitude *10)/10,1), \'\,\'), ROUND(round(oc.longitude *10)/10,1)),'
+		,'CONCAT(CONCAT(ROUND(round(oc.latitude *100)/100,2), \'\,\'), ROUND(round(oc.longitude *100)/100,2)),'
+		,'CONCAT(CONCAT(ROUND(round(oc.latitude *1000)/1000,3), \'\,\'), ROUND(round(oc.longitude *1000)/1000,3)),'
+		,'CONCAT(CONCAT(ROUND(round(oc.latitude *10000)/10000,4), \'\,\'), ROUND(round(oc.longitude *10000)/10000,4))'
+		,',IFNULL(oc.`year`,\'\'),IFNULL(oc.`month`,\'\'),IFNULL(DATE_FORMAT(oc.occurrence_date,\'%Y-%m-%dT%H:%i:%sZ\'),\'\'),'
+		,'IFNULL(oc.basis_of_record,\'\'),IFNULL(bor.description,\'\'),IFNULL(ror.basis_of_record,\'\'),'
+		,'IFNULL(get_type_status(oc.id),\'\'),IFNULL(get_identifier_type(oc.id),\'\'),IFNULL(get_identifier_name(oc.id),\'\'),'
+		,'IFNULL(ror.identifier_name,\'\'),IFNULL(DATE_FORMAT(ror.identification_date,\'%Y-%m-%dT%H:%i:%sZ\'),\'\'),'
+		,'IFNULL(ror.collector_name,\'\'),IFNULL(oc.taxonomic_issue,\'\'),IFNULL(oc.geospatial_issue,\'\'),'
+		,'IFNULL(oc.other_issue,\'\'),IFNULL(DATE_FORMAT(ror.created,\'%Y-%m-%dT%H:%i:%sZ\'),\'\'),IFNULL(DATE_FORMAT(ror.modified,\'%Y-%m-%dT%H:%i:%sZ\'),\'\'),'
+		,'IFNULL(ror.citation,\'\'), IFNULL(ror.occurrence_remarks,\'\'), IFNULL(ror.location_remarks,\'\'),'
+		,'IFNULL(ror.vernacular_name,\'\'), IFNULL(ror.record_number,\'\'), IFNULL(ror.identification_qualifier,\'\'), IFNULL(ror.individual_count,\'\'),'
+		,'IFNULL(ror.geodetic_datum,\'\'), IFNULL(ror.generalised_metres, \'\'), IFNULL(ror.user_id, \'\')'
+		,' FROM occurrence_record oc'
+		,' INNER JOIN raw_occurrence_record ror ON ror.id = oc.id'
+		,' INNER JOIN taxon_name tn ON tn.id = oc.taxon_name_id'
+		,' INNER JOIN taxon_concept tc ON tc.id = oc.nub_concept_id'
+		,' INNER JOIN rank rnk ON rnk.id = tc.rank'
+		,' INNER JOIN data_provider dp ON dp.id = oc.data_provider_id'
+		,' INNER JOIN data_resource dr ON dr.id = oc.data_resource_id'
+		,' INNER JOIN institution_code ic ON ic.id = oc.institution_code_id'
+		,' INNER JOIN collection_code cc ON cc.id = oc.collection_code_id'
+		,' INNER JOIN catalogue_number cn ON cn.id = oc.catalogue_number_id'
+		,' INNER JOIN basis_of_record bor ON bor.id = oc.basis_of_record'
+		,' LEFT JOIN inst_coll_mapping icm on icm.institution_code_id = oc.institution_code_id and icm.collection_code_id = oc.collection_code_id'
+		,' where dr.release_flag and oc.id >= ',low,' and oc.id < ', high
+		,' INTO OUTFILE '
+		,"'",'/data/bie-staging/biocache/tmp/file.out.', low,"'"
+		,' FIELDS TERMINATED BY \'\,\' OPTIONALLY ENCLOSED BY \'"\'');
+		PREPARE stmt1 FROM @cmd;
+		EXECUTE stmt1;
+		Deallocate prepare stmt1;
+		select concat('Finished processing ', low ,' ', now());
+		
+		select count(*) > 0 into more
+		from occurrence_record where id >= high;
+		
+		IF more THEN 
+			call dump_bio_occurrences(high, high + 1000000);
+		END IF;
+		
+	END;
+	$$
+
+	
 DELIMITER ;
 
-SELECT 'id','data_provider_id','data_provider','data_resource_id','data_resource',
-'institution_code_id','institution_code','institution_code_name','institution_code_lsid',
-'collection_code_id','collection_code','catalogue_number_id','catalogue_number',
-'taxon_concept_lsid','taxon_name','author', 'common_name', 'names_and_lsid','rank_id','rank','raw_taxon_name','raw_author','country_code',
-'lft','rgt','kingdom_lsid','kingdom','phylum_lsid','phylum','class_lsid','class','order_lsid','order',
-'family_lsid','family','genus_lsid','genus','species_lsid','species',
-'state',
-'biogeographic_region',
-'places',
-'latitude','longitude','lat_long_precision','lat_long',
-'cell_id','centi_cell_id','tenmilli_cell_id',
-'point-1','point-0.1','point-0.01','point-0.001','point-0.0001',
-'year','month','occurrence_date','basis_of_record_id','basis_of_record','raw_basis_of_record',
-'type_status','identifier_type','identifier_value','identifier_name','identifier_date',
-'collector','taxonomic_issue','geospatial_issue','other_issue','created_date','modified_date'
-UNION
-SELECT IFNULL(oc.id,''),IFNULL(oc.data_provider_id,''),IFNULL(dp.`name`,''),IFNULL(oc.data_resource_id,''),
-IFNULL(dr.`name`,''),IFNULL(oc.institution_code_id,''),IFNULL(ic.code,''),IFNULL(ic.`name`,''),
-IFNULL(ic.lsid,''),IFNULL(oc.collection_code_id,''),IFNULL(cc.code,''),IFNULL(oc.catalogue_number_id,''),
-IFNULL(cn.code,''),IFNULL(tc.guid,''),IFNULL(convert(convert(convert(tn.canonical USING latin1) USING binary) using utf8),''),IFNULL(tn.author,''), IFNULL(get_first_taxa_cn(oc.taxon_concept_id),''),
-CONCAT_WS('|', IFNULL(convert(convert(convert(tn.canonical USING latin1) USING binary) using utf8),''), IFNULL(tc.guid,''), IFNULL(get_first_taxa_cn(oc.taxon_concept_id),''), IFNULL(get_taxa_canonical(oc.kingdom_concept_id),''), IFNULL(get_taxa_canonical(oc.family_concept_id),'') ),
-IFNULL(tc.rank,''), IFNULL(rnk.`name`,''),IFNULL(ror.scientific_name,''),IFNULL(ror.author,''),
-IFNULL(oc.iso_country_code,''), IFNULL(tc.lft,''), IFNULL(tc.rgt,''),
-IFNULL(get_taxa_guid(oc.kingdom_concept_id),''), IFNULL(get_taxa_canonical(oc.kingdom_concept_id),''),
-IFNULL(get_taxa_guid(oc.phylum_concept_id),''), IFNULL(get_taxa_canonical(oc.phylum_concept_id),''),
-IFNULL(get_taxa_guid(oc.class_concept_id),''), IFNULL(get_taxa_canonical(oc.class_concept_id),''),
-IFNULL(get_taxa_guid(oc.order_concept_id),''), IFNULL(get_taxa_canonical(oc.order_concept_id),''),
-IFNULL(get_taxa_guid(oc.family_concept_id),''), IFNULL(get_taxa_canonical(oc.family_concept_id),''),
-IFNULL(get_taxa_guid(oc.genus_concept_id),''), IFNULL(get_taxa_canonical(oc.genus_concept_id),''),
-IFNULL(get_taxa_guid(oc.species_concept_id),''), IFNULL(get_taxa_canonical(oc.species_concept_id),''),
-IFNULL(get_georegion_names(oc.id,0,2),'') as states,
-IFNULL(get_georegion_names(oc.id,2000,3999),'') as bio_geo_regions,
-IFNULL(get_georegion_names(oc.id,3,11),'') as places,
-IFNULL(oc.latitude,''),IFNULL(oc.longitude,''),IFNULL(ror.lat_long_precision,''),
-IFNULL(CONCAT_WS(',', oc.latitude, oc.longitude),'') as lat_long,
-IFNULL(oc.cell_id,''),IFNULL(oc.centi_cell_id,''),IFNULL(oc.tenmilli_cell_id,''),
-CONCAT(CONCAT(ROUND(round(oc.latitude *1)/1,1), ','), ROUND(round(oc.longitude *1)/1,1)),
-CONCAT(CONCAT(ROUND(round(oc.latitude *10)/10,1), ','), ROUND(round(oc.longitude *10)/10,1)),
-CONCAT(CONCAT(ROUND(round(oc.latitude *100)/100,2), ','), ROUND(round(oc.longitude *100)/100,2)),
-CONCAT(CONCAT(ROUND(round(oc.latitude *1000)/1000,3), ','), ROUND(round(oc.longitude *1000)/1000,3)),
-CONCAT(CONCAT(ROUND(round(oc.latitude *10000)/10000,4), ','), ROUND(round(oc.longitude *10000)/10000,4)),
-IFNULL(oc.`year`,''),IFNULL(oc.`month`,''),IFNULL(DATE_FORMAT(oc.occurrence_date,'%Y-%m-%dT%H:%i:%sZ'),''),
-IFNULL(oc.basis_of_record,''),IFNULL(bor.description,''),IFNULL(ror.basis_of_record,''),
-IFNULL(get_type_status(oc.id),''),IFNULL(get_identifier_type(oc.id),''),IFNULL(get_identifier_name(oc.id),''),
-IFNULL(ror.identifier_name,''),IFNULL(DATE_FORMAT(ror.identification_date,'%Y-%m-%dT%H:%i:%sZ'),''),
-IFNULL(ror.collector_name,''),IFNULL(oc.taxonomic_issue,''),IFNULL(oc.geospatial_issue,''),
-IFNULL(oc.other_issue,''),IFNULL(DATE_FORMAT(ror.created,'%Y-%m-%dT%H:%i:%sZ'),''),IFNULL(DATE_FORMAT(ror.modified,'%Y-%m-%dT%H:%i:%sZ'),'')
-FROM occurrence_record oc
-INNER JOIN raw_occurrence_record ror ON ror.id = oc.id
-INNER JOIN taxon_name tn ON tn.id = oc.taxon_name_id
-INNER JOIN taxon_concept tc ON tc.id = oc.nub_concept_id
-INNER JOIN rank rnk ON rnk.id = tc.rank
-INNER JOIN data_provider dp ON dp.id = oc.data_provider_id
-INNER JOIN data_resource dr ON dr.id = oc.data_resource_id
-INNER JOIN institution_code ic ON ic.id = oc.institution_code_id
-INNER JOIN collection_code cc ON cc.id = oc.collection_code_id
-INNER JOIN catalogue_number cn ON cn.id = oc.catalogue_number_id
-INNER JOIN basis_of_record bor ON bor.id = oc.basis_of_record
---WHERE oc.data_resource_id = 56
-INTO outfile '/data/bie-staging/biocache/occurrences.csv'
---INTO outfile '/data/bie-staging/biocache/occurrences.56.csv'
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"' LINES TERMINATED BY '\n'; -- ESCAPED BY '"';
+--start at the lowest id for an occurrence record
+call dump_bio_occurrences(34588487, 35588487);
