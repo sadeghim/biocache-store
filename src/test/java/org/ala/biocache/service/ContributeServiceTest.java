367f83f4c766042e4498dc1380840e69ae5d0ff4
dcwmk282@gmail.com <dcwmk282@gmail.com>
2010-12-14 22:29:19 +0000

    test update function
    git-svn-id: http://ala-portal.googlecode.com/svn/branches/biocache-webapppostr5@1496 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- src/test/java/org/ala/biocache/service/ContributeServiceTest.java
+++ src/test/java/org/ala/biocache/service/ContributeServiceTest.java
@@ -9,12 +9,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ContributeServiceTest extends TestCase {
-
-	public void testRecordSighting(){
+	Sighting s;
+	ApplicationContext ctx;
+	ContributeService cs;
+	String guid = "" + System.currentTimeMillis();
+	 
+	protected void setUp(){
+		s = new Sighting();
+		ctx = new ClassPathXmlApplicationContext(new String[]{"classpath*:applicationContext-webapp.xml"});
+		cs = ctx.getBean(ContributeService.class);
 		
-		ApplicationContext ctx = new ClassPathXmlApplicationContext(new String[]{"classpath*:applicationContext-webapp.xml"});
-		ContributeService cs = ctx.getBean(ContributeService.class);
-		Sighting s = new Sighting();
 		s.setLocality("Clunes Ross");
 		s.setStateProvince("Australian Capital Territory");
 		s.setCollectorName("David Martin");
@@ -28,7 +32,19 @@ public class ContributeServiceTest extends TestCase {
 		s.setTaxonConceptGuid("urn:lsid:biodiversity.org.au:afd.taxon:aa745ff0-c776-4d0e-851d-369ba0e6f537");
 		s.setUserId("David.Martin@csiro.au");
 		s.setOccurrenceRemarks("It was a cold dark night....");
-		s.setEventDate(new Date());
-		cs.recordSighting(s);
+		s.setEventDate(new Date());				
+	}
+	
+	public void testRecordSighting(){
+		s.setGuid(guid);
+		assertTrue(cs.recordSighting(s));
+	}
+	
+	public void testupdateSighting(){
+		s.setGuid(guid);
+		s.setUserId("waiman.mokn@csiro.au");
+		s.setLatitude(-100.00f);
+		s.setLongitude(100.00f);
+		cs.updateSighting(s);
 	}
 }
