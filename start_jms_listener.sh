8b918c7610d97dd08e96f14ff327db2c4754f515
dcwmk282@gmail.com <dcwmk282@gmail.com>
2010-12-16 05:52:15 +0000

    clean up
    git-svn-id: http://ala-portal.googlecode.com/svn/branches/ala-jms@1508 1ef1b718-b4f5-11dd-90a5-ed332e521da9

--- start_jms_listener.sh
+++ start_jms_listener.sh
@@ -3,4 +3,4 @@ jar xf ala-jms-1.0-SNAPSHOT-assembly.jar lib lib
 export CLASSPATH=ala-jms-1.0-SNAPSHOT-assembly.jar
 
 echo "JMS Listener : running ActiveMQ JMS Listener $('date')"
-java -classpath $CLASSPATH org.ala.jms.SpringClient
+java -classpath $CLASSPATH org.ala.jms.JmsListenerService
